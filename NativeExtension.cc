9ea42bd2ef2267297068c917e6c753096baf0d34
Fabian Canas <fcanas@gmail.com>
2015-05-23 22:15:33 -0400

    Add top level comments to C++ files

--- NativeExtension.cc
+++ NativeExtension.cc
@@ -7,6 +7,9 @@ using v8::Handle;
 using v8::Object;
 using v8::String;
 
+// NativeExtensio.cc represents the top levle of the module. 
+// C++ constructs that are exposed to javascript are exported here
+
 void InitAll(Handle<Object> exports) {
   exports->Set(NanNew<String>("nothing"),
     NanNew<FunctionTemplate>(nothing)->GetFunction());
--- functions.h
+++ functions.h
@@ -3,6 +3,9 @@
 
 #include <nan.h>
 
+// Example top-level functions. These functions demonstrate how to return various js types.
+// Implementations are in functions.cc
+
 NAN_METHOD(nothing);
 NAN_METHOD(aString);
 NAN_METHOD(aBoolean);
