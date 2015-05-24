e0aacda2a947fb6cb0aad568ffa5e5fc417c35d2
Fabian Canas <fcanas@gmail.com>
2015-05-23 23:35:41 -0400

    Fix indentation

--- functions.cc
+++ functions.cc
@@ -2,50 +2,50 @@
 #include "functions.h"
 
 NAN_METHOD(nothing) {
-  NanScope();
-  NanReturnUndefined();
+    NanScope();
+    NanReturnUndefined();
 }
 
 NAN_METHOD(aString) {
-	NanScope();
-	NanReturnValue(NanNew<v8::String>("This is a thing."));
+    NanScope();
+    NanReturnValue(NanNew<v8::String>("This is a thing."));
 }
 
 NAN_METHOD(aBoolean) {
-	NanScope();
-	NanReturnValue(NanFalse());
+    NanScope();
+    NanReturnValue(NanFalse());
 }
 
 NAN_METHOD(aNumber) {
-	NanScope();
-	NanReturnValue(NanNew<v8::Number>(1.75));
+    NanScope();
+    NanReturnValue(NanNew<v8::Number>(1.75));
 }
 
 NAN_METHOD(anObject) {
-	NanScope();
-	v8::Local<v8::Object> obj = NanNew<v8::Object>();
-	obj->Set(NanNew<v8::String>("key"), NanNew<v8::String>("value")); 
-	NanReturnValue(obj);
+    NanScope();
+    v8::Local<v8::Object> obj = NanNew<v8::Object>();
+    obj->Set(NanNew<v8::String>("key"), NanNew<v8::String>("value")); 
+    NanReturnValue(obj);
 }
 
 NAN_METHOD(anArray) {
-	NanScope();
-	v8::Local<v8::Array> arr = NanNew<v8::Array>(3);
-	arr->Set(0, NanNew<v8::Number>(1));
-	arr->Set(1, NanNew<v8::Number>(2));
-	arr->Set(2, NanNew<v8::Number>(3));
-	NanReturnValue(arr);
+    NanScope();
+    v8::Local<v8::Array> arr = NanNew<v8::Array>(3);
+    arr->Set(0, NanNew<v8::Number>(1));
+    arr->Set(1, NanNew<v8::Number>(2));
+    arr->Set(2, NanNew<v8::Number>(3));
+    NanReturnValue(arr);
 }
 
 NAN_METHOD(callback) {
- 	NanScope();
+    NanScope();
 
- 	v8::Local<v8::Function> callbackHandle = args[0].As<v8::Function>();
-	NanCallback *callback = new NanCallback(callbackHandle);
+    v8::Local<v8::Function> callbackHandle = args[0].As<v8::Function>();
+    NanCallback *callback = new NanCallback(callbackHandle);
 
-	callback->Call(0, NULL);
+    callback->Call(0, NULL);
 
-	delete callback;
+    delete callback;
 
   NanReturnUndefined();
 }
