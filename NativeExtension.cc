a23fedf9f9f5eeaaf70d27ccba488666b417fb34
Panu Horsmalahti <panu.horsmalahti@iki.fi>
2015-05-24 22:40:33 +0300

    Fix a few typos.

--- NativeExtension.cc
+++ NativeExtension.cc
@@ -7,7 +7,7 @@ using v8::Handle;
 using v8::Object;
 using v8::String;
 
-// NativeExtensio.cc represents the top levle of the module. 
+// NativeExtension.cc represents the top level of the module. 
 // C++ constructs that are exposed to javascript are exported here
 
 void InitAll(Handle<Object> exports) {
